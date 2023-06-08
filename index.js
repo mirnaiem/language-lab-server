a4bce0b4fb7f000f65c5e8fe1e5261478e8691f4
mirnaiem <mirnaiem8teen@gmail.com>
2023-06-08 14:17:53 +0600

    resolve conflict

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app=express();
+const port=process.env.PORT || 3000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+ res.send('assignment 12 on the way')
+})
+app.listen(port)
